89ad584f6eac30a7b4054423d7f42f4c82d12a09
Fazri Egi <71480242+Fazri1@users.noreply.github.com>
2022-12-13 16:48:02 +0700

    Update yt-playlist-download.py

--- yt-playlist-download.py
+++ yt-playlist-download.py
@@ -1,42 +1,31 @@
+from pytube import YouTube
+from pytube import Playlist
 import os
-import subprocess
+import moviepy.editor as mp
+import re
+import sys
 
-from pytube import Playlist, YouTube
 
-def run(pl):
-    # get parent directory; VERY IMPORTANT!!
-    # INCLUDE LAST SLASH AFTER FOLDER NAME
-    # e.g. /home/username/Folder/ or C:\Users\Username\Folder\
-    filepath = input("Please enter the filepath of the directory where this script is located:\n")
-    # get linked list of links in the playlist
-    links = pl.video_urls
-    # download each item in the list
-    for l in links:
-        # converts the link to a YouTube object
-        yt = YouTube(l)
-        # takes first stream; since ffmpeg will convert to mp3 anyway
-        music = yt.streams.first()
-        # gets the filename of the first audio stream
-        default_filename = music.default_filename
-        print("Downloading " + default_filename + "...")
-        # downloads first audio stream
-        music.download()
-        # creates mp3 filename for downloaded file
-        # adjust the length of the downloaded video extension
-        # example, .3gpp = [0:-4]
-        new_filename = default_filename[0:-4] + "mp3"
-        print("Converting to mp3....")
-        # converts mp4 audio to mp3 audio
-        subprocess.run(['ffmpeg', '-i', 
-            os.path.join(filepath, default_filename),
-            # used 192kbps
-            '-b:a', '192K',
-            os.path.join(filepath, new_filename)
-        ])
+if len(sys.argv) != 3:
+    print(f'(+) Usage: {sys.argv[0]} <url> <output>.')
+    print(f'(+) Example: {sys.argv[0]} https://youtube.com/ /home/user/')
     
-    print("Download finished.")
+path = sys.argv[2]
+url = sys.argv[1]
 
-if __name__ == "__main__":
-    url = input("Please enter the url of the playlist you wish to download: ")
-    pl = Playlist(url)
-    run(pl)
+playlist = Playlist(url)
+
+#prints address of each YouTube object in the playlist
+for vid in playlist.videos:
+    print(vid)
+    
+for url in playlist:
+    YouTube(url).streams.first().download(path)
+
+for file in os.listdir(path):
+    if re.search('mp4', file):
+        mp4_path = os.path.join(path,file)
+        mp3_path = os.path.join(path,os.path.splitext(file)[0]+'.mp3')
+        new_file = mp.AudioFileClip(mp4_path)
+        new_file.write_audiofile(mp3_path)
+        os.remove(mp4_path)
