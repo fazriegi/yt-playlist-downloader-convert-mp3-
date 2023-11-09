a6118dea24ca86e2783e81d9c39991b415e583cd
fazriegi <fazriegi80@gmail.com>
2023-11-09 20:44:59 +0700

    add search for 3gpp file

--- yt-playlist-download.py
+++ yt-playlist-download.py
@@ -9,21 +9,18 @@ import sys
 if len(sys.argv) != 3:
     print(f'(+) Usage: {sys.argv[0]} <url> <output>.')
     print(f'(+) Example: {sys.argv[0]} https://youtube.com/ /home/user/')
+    os._exit(1)
     
 path = sys.argv[2]
 url = sys.argv[1]
 
 playlist = Playlist(url)
-
-#prints address of each YouTube object in the playlist
-for vid in playlist.videos:
-    print(vid)
     
 for url in playlist:
     YouTube(url).streams.first().download(path)
 
 for file in os.listdir(path):
-    if re.search('mp4', file):
+    if re.search('mp4', file) or re.search('3gpp', file):
         mp4_path = os.path.join(path,file)
         mp3_path = os.path.join(path,os.path.splitext(file)[0]+'.mp3')
         new_file = mp.AudioFileClip(mp4_path)
